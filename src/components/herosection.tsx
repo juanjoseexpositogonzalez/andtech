2b08942d19c85f6b2fba02c8f7acf774fbf924d8
jjeg1979 <jjeg1979@gmail.com>
2024-12-19 10:22:07 +0100

    Added footer to herosection

--- src/components/herosection.tsx
+++ src/components/herosection.tsx
@@ -2,9 +2,16 @@
 import React from 'react';
 import Image from 'next/image';
 
+const logos = [
+    '/mango-4.png',
+    '/brother.png',
+    '/gaastra.png',
+    '/eco.png',
+];
+
 const HeroSection: React.FC = () => {
     return (
-        <section className="px-8 py-16 flex items-center justify-center">
+        <section className="px-8 py-16 flex flex-col items-center justify-center">
             <div className="max-w-7xl w-full flex flex-col md:flex-row items-center">
                 {/* Texto del hero */}
                 <div className="flex-1 text-left">
@@ -33,6 +40,24 @@ const HeroSection: React.FC = () => {
                         height={500}
                         className="rounded-2xl shadow-lg"
                     />
+                </div>
+            </div>
+
+            {/* Footer del hero (They trust us) */}
+            <div className="mt-12 text-left w-full max-w-7xl">
+                <p className="text-gray-600 text-lg font-medium">They trust us:</p>
+                <div className="flex justify-left iterms-center flex-wrap gap-8 mt-4">
+                    {logos.map((logo, index) => (
+                        <div key={index} className="w-28 h-auto">
+                            <Image 
+                                src={logo}
+                                alt={`logo ${index +1}`}
+                                width={120}
+                                height={60}
+                                className="object-contain"
+                            />
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
